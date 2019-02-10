dc69fe708d594232704b79b73fafb3f415358d8b
Jphoenix <jordiyanki906@gmail.com>
2019-02-10 18:06:04 +0100

    mofication animation of sidebar

--- bootstrap/js/core.js
+++ bootstrap/js/core.js
@@ -1,9 +1,16 @@
 var sideBar=function(){
     var trigger=document.querySelector("#sidebar_Trigger"),
     sidebar=document.querySelector("#sidebar");
+    content=document.querySelector(".content");
 
     trigger.addEventListener("click",function(e){
-        sidebar.classList.toggle("show_sidebar");   
+        sidebar.classList.toggle("show_sidebar");
+        if(sidebar.classList.contains("show_sidebar")){
+            content.style.display="none";
+        }else{
+            content.style.display="block"; 
+        }
+
     })
 }
 sideBar();
