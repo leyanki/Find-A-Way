13fbb81c6f322610cc0aaed704b6e80a24cfcbf0
Jphoenix <jordiyanki906@gmail.com>
2019-02-23 02:02:08 +0100

    left side

--- bootstrap/js/core.js
+++ bootstrap/js/core.js
@@ -1,17 +1,15 @@
-var sideBar=function(){
-    var trigger=document.querySelector("#sidebar_Trigger"),
-    sidebar=document.querySelector("#sidebar");
-    content=document.querySelector(".content");
-
-    trigger.addEventListener("click",function(e){
-        sidebar.classList.toggle("show_sidebar");
-        if(sidebar.classList.contains("show_sidebar")){
-            content.style.display="none";
-        }else{
-            content.style.display="block"; 
-        }
-
-    })
+var sidebar=function(){
+    var sidebar=document.querySelector("#sideBarResponsive"),
+        triggerSlide=document.querySelector("#sidebar_Trigger");
+        triggerSlide.addEventListener("click", function(e){
+            if(!sidebar.classList.contains("show_side")){
+                sidebar.classList.add("show_side");
+            }
+        })
+        sidebar.addEventListener("click",function(e){
+            if(sidebar.classList.contains("show_side")){
+                sidebar.classList.remove("show_side");  
+            }
+        })
 }
-sideBar();
-    
+sidebar();
